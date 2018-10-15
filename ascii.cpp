b4f2050c49b7b35fb67f432ae20cde86152424f1
Shivam-nikhil <44004764+Shivam-nikhil@users.noreply.github.com>
2018-10-16 00:05:55 +0530

    Create ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char ch;
+int a;
+cout<<"enter any character";
+cin>>ch;
+cout<<"ascii value of character is:";
+a=ch;
+cout<<a;
+return 0;
+}
